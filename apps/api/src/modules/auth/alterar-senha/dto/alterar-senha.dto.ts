15ec7db4d94054e45815f075d961a2ec35d7f565
MamayCarvalho <115594676+MamayCarvalho@users.noreply.github.com>
2026-08-03 21:51:07 -0300

    Implementação da alteração de senha

--- apps/api/src/modules/auth/alterar-senha/dto/alterar-senha.dto.ts
+++ apps/api/src/modules/auth/alterar-senha/dto/alterar-senha.dto.ts
@@ -1,1 +1,15 @@
-export class AlterarSenhaDto {}
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class AlterarSenhaDto {
+    @IsString()
+    @IsNotEmpty()
+    senhaAtual!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8, {
+        message: 'A senha deve possuir no mínimo 8 caracteres.',
+    })
+    senhaNova!: string;
+
+}
